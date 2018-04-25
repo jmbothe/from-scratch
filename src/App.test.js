5e7c8b4cb66fcaa554e89024adb665d1331be971
jmbothe <jeffrymbothe@gmail.com>
2018-04-25 09:55:13 -0400

    test: Write tests for <App /> render

--- src/App.test.js
+++ src/App.test.js
@@ -9,4 +9,20 @@ describe('<App />', () => {
   it('renders correctly', () => {
     expect(app).toMatchSnapshot();
   })
+
+  it('renders a <Header />', () => {
+    expect(app.find('Header').exists()).toBe(true)
+  })
+
+  it('contains a redux-connected <NewTodo />', () => {
+    expect(app.find('Connect(NewTodo)').exists()).toBe(true)
+  })
+
+  it('renders a redux-connected <Todos />', () => {
+    expect(app.find('Connect(Todos)').exists()).toBe(true)
+  })
+
+  it('renders a redux-connected <ViewFilters />', () => {
+    expect(app.find('Connect(ViewFilters)').exists()).toBe(true)
+  })
 })
